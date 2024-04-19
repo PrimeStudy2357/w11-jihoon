ac9b91ad3f283db67820e3a3e1922a379a6e7567
anteater@mac <anteater1056@gmail.com>
2024-04-19 15:25:29 +0900

    서버 개선

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const PORT = 3000;
  * 요청을 받으면 Callback 함수에 지정한 로직을 실행하는 서버
  */
 const server = http.createServer((request, response) => {
-  response.writeHead(200);
-  response.end(
-    `<div><h1>Hello, World!</h1><p>Current Time : ${new Date()}</p></div>`
+  const now = new Date();
+  console.log(
+    `${now} :: 요청이 들어왔습니다. (요청지${request.socket.remoteAddress})`
   );
+
+  response.writeHead(200, {
+    "Content-Type": "text/html; charset=utf-8",
+  });
+
+  response.end(`<div><h1>Hello, World!</h1><p>Current Time : ${now}</p></div>`);
 });
 
 // 서버 실행
